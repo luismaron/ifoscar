4d3938b3bf5f312202e7b555bd34636ff4e3f3f0
João Vitor Lima <jvolima2004@gmail.com>
2022-08-10 10:34:50 -0300

    Atualizar video clipes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import { CreateStudentController } from './controllers/CreateStudentController';
 import { CreateVideoClipController } from './controllers/CreateVideoClipController';
 import ImportStudentsController from './controllers/ImportStudentsController';
 import { ListVideoClipsController } from './controllers/ListVideoClipsController';
+import { UpdateVideoClipController } from './controllers/UpdateVideoClipController';
 
 const routes = Router();
 
@@ -16,6 +17,7 @@ const importStudentsController = new ImportStudentsController();
 const createStudentController = new CreateStudentController();
 const createVideoClipController = new CreateVideoClipController();
 const listVideoClipsController = new ListVideoClipsController();
+const updateVideoClipController = new UpdateVideoClipController();
 
 routes.post(
   '/students/upload',
@@ -28,5 +30,7 @@ routes.post('/students/create', createStudentController.handle);
 routes.post('/videoclips/create', createVideoClipController.handle);
 
 routes.get('/videoclips', listVideoClipsController.handle);
+
+routes.put('/videoclips/:id', updateVideoClipController.handle);
 
 export { routes };
---
+++ backend/src/services/UpdateVideoClipService.ts
@@ -0,0 +1,51 @@
+import { AppError } from '../core/domain/errors/AppError';
+import { prisma } from '../database';
+
+interface IUpdateVideoClipService {
+  id: string;
+  name: string;
+  link: string;
+  students: {
+    id: string;
+  }[];
+}
+
+class UpdateVideoClipService {
+  async execute({ id, name, link, students }: IUpdateVideoClipService) {
+    const videoClip = await prisma.videoClip.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        students: true,
+      },
+    });
+
+    if (!videoClip) {
+      throw new AppError('VideoClip does not exists', 404);
+    }
+
+    // const idsToBeDisconnected = videoClip.
+
+    const ids = students.map((student) => {
+      return {
+        id: student.id,
+      };
+    });
+
+    await prisma.videoClip.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        link,
+        students: {
+          set: ids,
+        },
+      },
+    });
+  }
+}
+
+export { UpdateVideoClipService };
