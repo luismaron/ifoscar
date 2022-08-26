244e455239a1cfc6b17836ad4e52bdc66df91b3b
João Vitor Lima <jvolima2004@gmail.com>
2022-08-26 09:40:23 -0300

    Rota de listagem de atores e atrizes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import 'express-async-errors';
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 
 import { AppError } from './core/domain/errors/AppError';
@@ -6,6 +7,7 @@ import { routes } from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
+	useEffect(() => {
+		fetch("http://localhost:3334/videoclips").then(response => console.log(response));
+	}, []);
+
 	return (
 		<div>
-			<h2>Hello World</h2>
+			<h1>Ifoscar</h1>
+
+			<h2>Melhor videoclipes</h2>
+
+			<div>
+				<div>
+					<h3>Exagerado</h3>
+				</div>
+
+				<div>
+					<h3>Dormi na praça</h3>
+				</div>
+			</div>
 		</div>
 	);
 }
