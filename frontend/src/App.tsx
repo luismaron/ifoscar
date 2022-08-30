a7aa19a52c178f4d99b18fa7c1f4075ab8c6e1d3
João Vitor Lima <jvolima2004@gmail.com>
2022-08-29 22:49:21 -0300

    Enviando voto para a API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,11 +27,17 @@ interface Vote {
 }
 
 function App() {
+	const [registration, setRegistration] = useState("");
 	const [actors, setActors] = useState<Actor[]>([]);
 	const [videoClips, setVideoClips] = useState<VideoClip[]>([]);
-	const [vote, setVote] = useState<Vote>({} as Vote);
 
 	const [selectedActor, setSelectedActor] = useState<Actor>({} as Actor);
+	const [selectedActress, setSelectedActress] = useState<Actor>({} as Actor);
+	const [selectedSupportingActor, setSelectedSupportingActor] = useState<Actor>({} as Actor);
+	const [selectedSupportingActress, setSelectedSupportingActress] = useState<Actor>({} as Actor);
+	const [selectedVideoClip, setSelectedVideoClip] = useState<VideoClip>({} as VideoClip);
+	const [selectedVideoClipCostume, setSelectedVideoClipCostume] = useState<VideoClip>({} as VideoClip);
+	const [selectedVideoClipEdition, setSelectedVideoClipEdition] = useState<VideoClip>({} as VideoClip);
 
 	useEffect(() => {
 		async function loadData() {
@@ -45,10 +51,25 @@ function App() {
 		loadData();
 	}, []);
 
-	function handleSubmitForm(evt: FormEvent) {
+	async function handleSubmitForm(evt: FormEvent) {
 		evt.preventDefault(); // Prevenir comportamento padrão do formulário
 
-		console.log(selectedActor);
+		try {
+			await api.post("/vote", {
+				student_registration: registration,
+				best_actor_id: selectedActor.id,
+				best_actress_id: selectedActress.id,
+				best_supporting_actor_id: selectedSupportingActor.id,
+				best_supporting_actress_id: selectedSupportingActress.id,
+				best_videoclip_costume_id: selectedVideoClipCostume.id,
+				best_videoclip_edition_id: selectedVideoClipEdition.id,
+				best_videoclip_id: selectedVideoClip.id
+			});
+
+			alert("Voto enviado!");
+		} catch (error) {
+			alert("Erro ao realizar voto.");
+		}
 	}
 
 	return (
@@ -65,6 +86,74 @@ function App() {
 						</div>
 					))
 				}
+
+				<h2>Melhor atriz</h2>
+				{
+					actors.filter(actor => actor.gender === "Female").map(actor => (
+						<div key={actor.id}>
+							<label htmlFor="actress">{actor.name}</label>
+							<input type="radio" name="actress" onClick={() => setSelectedActress(actor)} />
+						</div>
+					))
+				}
+
+				<h2>Melhor ator coadjuvante</h2>
+				{
+					actors.filter(actor => actor.gender === "Male").map(actor => (
+						<div key={actor.id}>
+							<label htmlFor="supportingActor">{actor.name}</label>
+							<input type="radio" name="supportingActor" onClick={() => setSelectedSupportingActor(actor)} />
+						</div>
+					))
+				}
+
+				<h2>Melhor atriz coadjuvante</h2>
+				{
+					actors.filter(actor => actor.gender === "Female").map(actor => (
+						<div key={actor.id}>
+							<label htmlFor="supportingActress">{actor.name}</label>
+							<input type="radio" name="supportingActress" onClick={() => setSelectedSupportingActress(actor)} />
+						</div>
+					))
+				}
+
+				<h2>Melhor video clipe</h2>
+				{
+					videoClips.map(videoClip => (
+						<div key={videoClip.id}>
+							<label htmlFor="videoClip">{videoClip.name}</label>
+							<input type="radio" name="videoClip" onClick={() => setSelectedVideoClip(videoClip)} />
+						</div>
+					))
+				}
+
+				<h2>Melhor figurino</h2>
+				{
+					videoClips.map(videoClip => (
+						<div key={videoClip.id}>
+							<label htmlFor="videoClipCostume">{videoClip.name}</label>
+							<input type="radio" name="videoClipCostume" onClick={() => setSelectedVideoClipCostume(videoClip)} />
+						</div>
+					))
+				}
+
+				<h2>Melhor edição</h2>
+				{
+					videoClips.map(videoClip => (
+						<div key={videoClip.id}>
+							<label htmlFor="videoClipEdition">{videoClip.name}</label>
+							<input type="radio" name="videoClipEdition" onClick={() => setSelectedVideoClipEdition(videoClip)} />
+						</div>
+					))
+				}
+
+				<label htmlFor="registration">Matrícula</label>
+				<input 
+					type="text" 
+					placeholder="Insira seu número de matrícula" 
+					name="registration" value={registration} 
+					onChange={(evt) => setRegistration(evt.target.value)} 
+				/>
 
 				<button type="submit">Enviar</button>
 			</form>
