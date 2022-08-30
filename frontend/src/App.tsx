df6db16b8ae4721889002167b2d6ce7101f59df0
João Vitor Lima <jvolima2004@gmail.com>
2022-08-29 21:44:05 -0300

    Escolhendo melhor ator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,73 @@
-import React, { useEffect } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
+import { api } from "./services/api";
+
+interface Actor {
+	id: string;
+	name: string;
+	registration: string;
+	gender: "Male" | "Female";
+}
+
+interface VideoClip {
+	id: string;
+	name: string;
+	link: string;
+	actors: Actor[];
+}
+
+interface Vote {
+	student_registration: string;
+	best_actor_id: string;
+  best_actress_id: string;
+  best_supporting_actor_id: string;
+  best_supporting_actress_id: string;
+  best_videoclip_costume_id: string;
+  best_videoclip_edition_id: string;
+  best_videoclip_id: string;
+}
 
 function App() {
+	const [actors, setActors] = useState<Actor[]>([]);
+	const [videoClips, setVideoClips] = useState<VideoClip[]>([]);
+	const [vote, setVote] = useState<Vote>({} as Vote);
+
+	const [selectedActor, setSelectedActor] = useState<Actor>({} as Actor);
+
 	useEffect(() => {
-		fetch("http://localhost:3334/videoclips").then(response => console.log(response));
+		async function loadData() {
+			const responseActors = await api.get("/actors");
+			setActors(responseActors.data);
+
+			const responseVideoClips = await api.get("/videoclips");
+			setVideoClips(responseVideoClips.data);
+		}
+
+		loadData();
 	}, []);
+
+	function handleSubmitForm(evt: FormEvent) {
+		evt.preventDefault(); // Prevenir comportamento padrão do formulário
+
+		console.log(selectedActor);
+	}
 
 	return (
 		<div>
 			<h1>Ifoscar</h1>
 
-			<h2>Melhor videoclipes</h2>
+			<form onSubmit={handleSubmitForm}>
+				<h2>Melhor ator</h2>
+				{
+					actors.filter(actor => actor.gender === "Male").map(actor => (
+						<div key={actor.id}>
+							<label htmlFor="actor">{actor.name}</label>
+							<input type="radio" name="actor" onClick={() => setSelectedActor(actor)} />
+						</div>
+					))
+				}
 
-			<div>
-				<div>
-					<h3>Exagerado</h3>
-				</div>
-
-				<div>
-					<h3>Dormi na praça</h3>
-				</div>
-			</div>
+				<button type="submit">Enviar</button>
+			</form>
 		</div>
 	);
 }
