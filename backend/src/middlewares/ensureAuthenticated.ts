82c00047a4e86c8ada3f8df6d763d30e0e5b1209
João Vitor Lima <jvolima2004@gmail.com>
2022-09-05 22:08:35 -0300

    Autenticação do administrador

---
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '../core/domain/errors/AppError';
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing.', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  const secretToken = process.env.JWT_SECRET as string;
+
+  try {
+    const { sub: user_id } = verify(token, secretToken);
+
+    request.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch (error) {
+    throw new AppError('Token invalid.', 401);
+  }
+}
